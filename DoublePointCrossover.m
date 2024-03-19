d40124da8274747889adfebc9e65bb27e18b0da4
Najeeb Ullah <78012875+NajeebClouds@users.noreply.github.com>
2024-03-19 12:34:43 +0800

    Update DoublePointCrossover.m

--- DoublePointCrossover.m
+++ DoublePointCrossover.m
@@ -1,16 +1,3 @@
-%
-% Copyright (c) 2015, Yarpiz (www.yarpiz.com)
-% All rights reserved. Please read the "license.txt" for license terms.
-%
-% Project Code: YPEA101
-% Project Title: Implementation of Binary Genetic Algorithm in MATLAB
-% Publisher: Yarpiz (www.yarpiz.com)
-% 
-% Developer: S. Mostapha Kalami Heris (Member of Yarpiz Team)
-% 
-% Contact Info: sm.kalami@gmail.com, info@yarpiz.com
-%
-
 function [y1, y2]=DoublePointCrossover(x1,x2)
 
     nVar=numel(x1);
